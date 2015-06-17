63fac08f9eece7e14e75db959a6dce31eb71ec76
Kristian Lyngbæk <klyngbaek@users.noreply.github.com>
2015-06-17 12:59:59 -0700

    Clarified var names

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ module.exports = function (chunkSize) {
     // buffer to store the last few bytes of incoming data
     // if it does not divide evenly into chunkSize
     var buffer = new Buffer(0);
-    return through2(function (data, enc, next) {
-        data = Buffer.concat([buffer, data]);
-        var totalLength = data.length;
+    return through2(function (newData, enc, next) {
+        var allData = Buffer.concat([buffer, newData]);
+        var totalLength = allData.length;
         var remainder = totalLength % chunkSize;
         var cutoff = totalLength - remainder;
         for (var i=0 ; i<cutoff ; i+=chunkSize) {
-            var chunk = data.slice(i, i+chunkSize);
+            var chunk = allData.slice(i, i+chunkSize);
             this.push(chunk);
         }
-        buffer = data.slice(cutoff, totalLength);
+        buffer = allData.slice(cutoff, totalLength);
         next();
     });
 }
