274653670b62103d990fc5038000c00118d87a6c
Kristian Lyngbæk <klyngbaek@users.noreply.github.com>
2015-06-17 12:55:12 -0700

    Clarified var names

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ var through2 = require('through2');
 
 module.exports = ByteBatcher;
 
-function ByteBatcher (size) {
+function ByteBatcher (chunkSize) {
     // buffer to store the last few bytes of incoming data
-    // if it does not divide evenly into size
+    // if it does not divide evenly into chunkSize
     var buffer = new Buffer(0);
     return through2(function (data, enc, next) {
         data = Buffer.concat([buffer, data]);
-        var length = data.length;
-        var remainder = length%size;
-        var cutoff = length - remainder;
-        for (var i=0 ; i<cutoff ; i+= size) {
-            var chunk = data.slice(i, i+size);
+        var totalLength = data.length;
+        var remainder = totalLength%chunkSize;
+        var cutoff = totalLength - remainder;
+        for (var i=0 ; i<cutoff ; i+=chunkSize) {
+            var chunk = data.slice(i, i+chunkSize);
             this.push(chunk);
         }
-        buffer = data.slice(cutoff, length);
+        buffer = data.slice(cutoff, totalLength);
         next();
     });
 }
